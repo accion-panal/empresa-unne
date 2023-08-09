dc09f3597c29842b969a6d50d9cf63e5a893b99c
Nicolas Cabrera <nicolascabreramillacan@gmail.com>
2023-08-09 17:56:33 -0400

    feat: add animation in logo unne with svg

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import UnneLogo from '../../assets/img/logo/unne-logo.png';
+import UnneLogoSvg from '../Icons/custom/unneLogo';
 
 const Logo = () => {
   return (
     <Link to="/">
       {UnneLogo && (
-        <img src={UnneLogo} alt="unne-logo" width={150} height={100} />
+        <UnneLogoSvg></UnneLogoSvg>
       )}
     </Link>
   );
